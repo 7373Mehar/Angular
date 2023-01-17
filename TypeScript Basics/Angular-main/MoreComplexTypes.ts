0a73bfc19873e7ba161c7e7211742293377f52e3
Mehar Mutreja <meharpersonal01@gmail.com>
2023-01-17 17:13:33 +0530

    Typescript Files

---
+++ TypeScript Basics/Angular-main/MoreComplexTypes.ts
@@ -0,0 +1,24 @@
+//More Complex Types
+//Assigning Aliases
+
+let hobbies: string[];
+
+hobbies = ['Sports','Music','Reading Novels'];
+
+type Person = {
+    name: string;
+    age: number;
+};
+
+let person: Person
+
+person = {
+    name: 'Mehar',
+    age: 22
+};
+
+// person = {
+  //  isEmployee: true
+//};
+
+let people: Person[];
---
+++ TypeScript Basics/Angular-main/Primitives.ts
@@ -0,0 +1,16 @@
+let age: number;
+
+age = 4;
+
+let userName: string;
+
+userName = 'Mehar';
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+let hobbies: null;
+
+//hobbies = 'football';
+//Type '"football"' is not assignable to type 'null'
